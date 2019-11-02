2ad43b989b54f5976b3201546d0c6420c9a18e7d
saramarin23 <saramarin.econ@gmail.com>
2019-11-02 18:30:23 +0100

    add communication with the server

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ let express = require("express");
 //2º Iniciamos el servidor
 let app = express();
 
+//5º Cremos nuestra función handler
+//6º Queremos enviar un mensaje al cliente
+app.get("/", function(req, res) {
+  res.send("Yay Node Girls!");
+});
+
 //3º Escuchamos las peticiones potenciales
 app.listen(3000, function() {
   console.log("Server is listening on port 3000. Ready to accept requests!");
